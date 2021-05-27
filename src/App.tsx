b8e11728141b43bf57a1be1476f090ba5ef5e999
Kylian Vermeulen <email@kylianvermeulen.com>
2021-05-27 22:36:03 +0200

    build: add spotify-web-api module
    Signed-off-by: Kylian Vermeulen <email@kylianvermeulen.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 
 function App() {
+  const spotify = new SpotifyWebApi();
+  spotify.setPromiseImplementation(0);
+  // eslint-disable-next-line no-console
+  spotify.getMyCurrentPlayingTrack().then((obj) => console.log(obj));
   return (
     <div className="App">
       <header className="App-header">
